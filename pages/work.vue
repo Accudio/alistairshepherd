605d9457e3232af142e69fe2e1a7e9da4560544a
Alistair Shepherd <alistair@accudio.com>
2018-09-21 15:55:53 +0100

    Wrong URL in work page

--- pages/work.vue
+++ pages/work.vue
@@ -6,7 +6,7 @@
         I do freelance development under by brand <a href="https://accudio.com">Accudio</a> for individuals and small businesses around Edinburgh and the Highlands of Scotland.
       </p>
       <p>
-        You can find more about the work I do as Accudio on <a href="https://accudio.com">accudio.com</a>, particularly on the <a href="https://accudio.com/web/portfolio">portfolio</a>. If you are a business or individual looking for a bespoke website or website upgrade, please <a href="https://accudio.com/contact">get in touch</a>!
+        You can find more about the work I do as Accudio on <a href="https://accudio.com">accudio.com</a>, particularly on the <a href="https://accudio.com/web/portfolio">portfolio</a>. If you are a business or individual looking for a bespoke website or website upgrade, please <a href="https://accudio.com/about/contact/">get in touch</a>!
       </p>
       <p>
         I am also available for contract or full time work around Edinburgh. If you or your company are hiring I would love to hear from you!
